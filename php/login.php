3ace39521f2de009481e10d64d59a2c021831353
Sergio de la Rubia Garc?a-Carpintero <srug86@gmail.com>
2011-06-02 15:34:44 +0200

    implementados el registro y el login

--- php/login.php
+++ php/login.php
@@ -2,17 +2,19 @@
     $conexion = mysql_connect('chico.inf-cr.uclm.es','mhrv','mhrvpass') or die("Error al conectar con la BD");
     mysql_select_db('mhrv') or die ("Error al seleccionar la BD");
 
-    $consultaSQL = "SELECT * FROM JMTT_SRGC_usuario WHERE user = '".$_REQUEST['txtUser']."' AND password = '".$_REQUEST['txtPassword']."';";
+    $consultaUsuario = "SELECT * FROM JMTT_SRGC_usuario WHERE user = '".$_REQUEST['txtUser']."' AND password = '".$_REQUEST['txtPassword']."';";
 
-    $resultado = mysql_num_rows($consultaSQL) or die ("Error al realizar la consulta");
+    $resultado = mysql_query($consultaUsuario) or die ("Error al realizar la consulta");
 
-    if ($resultado != 0) {
+    $nfilas = mysql_num_rows($resultado);
+
+    if ($nfilas == 1) {
         echo "estado=correcto";
     }
     else {
         echo "estado=incorrecto";
     }
 
-    mysql_close($conexionSQL) or die ("Error al cerrar la conexión");
+    mysql_close($conexion) or die ("Error al cerrar la conexión");
 ?>
 
--- php/register.php
+++ php/register.php
@@ -2,9 +2,21 @@
     $conexion = mysql_connect('chico.inf-cr.uclm.es','mhrv','mhrvpass') or die("Error al conectar con la BD");
     mysql_select_db('mhrv') or die ("Error al seleccionar la BD");
 
-    $consultaSQL = "INSERT INTO JMTT_SRGC_usuario(user, password) VALUES ('".$_REQUEST['txtUser']."','".$_REQUEST['txtPassword']."');";
+    $consultaUsuario = "SELECT * FROM JMTT_SRGC_usuario WHERE user = '".$_REQUEST['txtUser']."';";
 
-    mysql_query($consultaSQL) or die ("Error al realizar la consulta");
+    $resultado = mysql_query($consultaUsuario) or die ("Error al realizar la consulta");
+
+    $nfilas = mysql_num_rows($resultado);
+
+    if ($nfilas == 0) {
+        echo "estado=correcto";
+        $consultaSQL = "INSERT INTO JMTT_SRGC_usuario(user, password) VALUES ('".$_REQUEST['txtUser']."','".$_REQUEST['txtPassword']."');";
+
+        mysql_query($consultaSQL) or die ("Error al realizar la consulta");
+    }
+    else {
+        echo "estado=incorrecto";
+    }
     mysql_close($conexion) or die ("Error al cerrar la conexión");
 ?>
 
